7abf8dbd0e48da9c31ed04b431a7cb3950b2acfb
tt142857 <timelimit12@naver.com>
2023-08-04 10:36:59 +0900

    https 적용

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const https = require('https');
+const { Certificate } = require('crypto');
 
 const app = express();
+const keys_dir = 'config/secure/';
+const ca = fs.readFileSync(keys_dir + 'ca_bundle.crt');
+const key = fs.readFileSync(keys_dir + 'private.key');
+const cert = fs.readFileSync(keys_dir + 'certificate.crt');
+const options = {key, cert, ca};
+const server = https.createServer(options, app);
 
-app.set('port', process.env.PORT || 3000);
+
+app.set('port', 80 || 443);
 
 global.contextPath = __dirname;
 
@@ -14,7 +24,7 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.use('/test', require('./routes/test/test.js'));
 
-app.listen(app.get('port'), () => {
+server.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 퐅에서 대기 중')
 });
 
